11187e43bc4f4b8a201227a845ccf7da42d08b45
Max M <gtmax.yo@gmail.com>
2021-11-05 05:31:53 +0100

    Added different random algorithms

--- index.js
+++ index.js
@@ -305,12 +305,81 @@ const input = (e,isEvent=true) => {
     
 }
 
+function xmur3(str) {
+    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+        h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
+        h = h << 13 | h >>> 19;
+    return function() {
+        h = Math.imul(h ^ h >>> 16, 2246822507);
+        h = Math.imul(h ^ h >>> 13, 3266489909);
+        return (h ^= h >>> 16) >>> 0;
+    }
+}
+function sfc32(a, b, c, d) {
+    return function() {
+      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+      var t = (a + b) | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      d = d + 1 | 0;
+      t = t + d | 0;
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+}
+
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
+function xoshiro128ss(a, b, c, d) {
+    return function() {
+        var t = b << 9, r = a * 5; r = (r << 7 | r >>> 25) * 9;
+        c ^= a; d ^= b;
+        b ^= c; a ^= d; c ^= t;
+        d = d << 11 | d >>> 21;
+        return (r >>> 0) / 4294967296;
+    }
+}
+
+function jsf32(a, b, c, d) {
+    return function() {
+        a |= 0; b |= 0; c |= 0; d |= 0;
+        var t = a - (b << 27 | b >>> 5) | 0;
+        a = b ^ (c << 17 | c >>> 15);
+        b = c + d | 0;
+        c = d + t | 0;
+        d = a + t | 0;
+        return (d >>> 0) / 4294967296;
+    }
+}
+
+// Create xmur3 state:
+var seed = xmur3("TAG-2022");
+// Output four 32-bit hashes to provide the seed for sfc32.
+var rand = sfc32(seed(), seed(), seed(), seed());
+
+// Output one 32-bit hash to provide the seed for mulberry32.
+// var rand = mulberry32(seed());
+
 
 
 function scanAll(S=settings, svgSize = APP.svg.element.viewBox.baseVal, useWorker=true) {
     // function scanAll(step={x:3,y:5}, noise = [1,3], isSin = true, svgSize = APP.svg.element.viewBox.baseVal, useWorker=true, faseK=.1) {
     let svg = document.querySelectorAll('svg > path')
     // svg.forEach(p=>p?.remove())
+    seed = xmur3("TAG-2022");
+
+    // rand = sfc32(seed(), seed(), seed(), seed());
+    rand = jsf32(seed(), seed(), seed(), seed());
+    // rand = xoshiro128ss(seed(), seed(), seed(), seed());
+    // rand = mulberry32(seed());
 
     const lines = []
     // console.log(arguments)
@@ -359,8 +428,8 @@ function scanLine(y, width = 100, S) {
     const nInc =  dnoise / (2*dx)
     const PI = Math.PI
     // const rand = Math.random
-    const rand = random(-100000)
-    const sin = isSin ? (x) => Math.sin(faseK*y+2*PI*x/step*rand()) : _=>rand()
+    // const rand = random(-100000)
+    const sin = isSin ? (x) => Math.sin(faseK*y+2*PI*x/step+rand()) : _=>1
     let isLogoOld = -2;
     let x1 = -1;
     let oldN = -1;
